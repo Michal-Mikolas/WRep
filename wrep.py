6d69038c55ddc19d7e56f9ff1a6f06d793417b9f
Michal Mikolas <nanuqcz@gmail.com>
2020-11-20 17:26:47 +0100

    typo

--- wrep.py
+++ wrep.py
@@ -6,6 +6,7 @@ from kivy.properties import ObjectProperty, StringProperty
 from datetime import date, timedelta
 import re
 import math
+# import numpy
 
 
 class MainView(BoxLayout):
@@ -40,25 +41,27 @@ class MainView(BoxLayout):
 				break
 
 			columns = line.split('\t')
+			id = columns[6]
 
-			if columns[6] not in stats:
-				stats[columns[6]] = {
+			if id not in stats:
+				stats[id] = {
 					'name': '',
 					'tasks': [],
 					'hours': 0.0,
 				}
 
 				# Stats: Category name
-				stats[columns[6]]['name'] = columns[6].strip()
+				stats[id]['name'] = id.strip()
 
 			# Stats: Hours
-			stats[columns[6]]['hours'] += float(columns[3])
+			stats[id]['hours'] += float(columns[3])
 
 			# Stats: Tasks
 			tasks = re.split(';\s*', columns[7].strip('; '))
-			stats[columns[6]]['tasks'] += tasks
-			stats[columns[6]]['tasks'] = list(set(stats[columns[6]]['tasks']))  # remove duplicates
-			# or: uniq = []; [uniq.append(x) for x in array if x not in uniq]
+			stats[id]['tasks'] += tasks
+			stats[id]['tasks'] = list(set(stats[id]['tasks']))  # remove duplicates
+			# or?: stats[id]['tasks'] = numpy.unique(stats[id]['tasks'])  # remove duplicates
+			# or?: uniq = []; [uniq.append(x) for x in array if x not in uniq]
 
 		#
 		# 2) Render
